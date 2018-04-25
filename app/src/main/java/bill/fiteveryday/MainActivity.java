6ecdabf3a36f9e988f1d7c32b45c27ca01c7861c
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2018-04-25 17:06:15 +0800

    0.0

--- app/src/main/java/bill/fiteveryday/MainActivity.java
+++ app/src/main/java/bill/fiteveryday/MainActivity.java
@@ -1,7 +1,14 @@
 package bill.fiteveryday;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button_push_ups);
+        Button button2 = (Button) findViewById(R.id.button_sit_ups);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumberDialog();
+            }
+        });
+    }
+
+    private void setNumberDialog() {
+        AlertDialog.Builder customizeDialog =
+                new AlertDialog.Builder(MainActivity.this);
+        final View dialogView = LayoutInflater.from(MainActivity.this)
+                .inflate(R.layout.set_number, null);
+        customizeDialog.setTitle("我是一个自定义Dialog");
+        customizeDialog.setView(dialogView);
+        customizeDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 获取EditView中的输入内容
+                        EditText edit_text =
+                                (EditText) dialogView.findViewById(R.id.edit_text);
+                        Toast.makeText(MainActivity.this,
+                                edit_text.getText().toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        customizeDialog.show();
     }
 }
