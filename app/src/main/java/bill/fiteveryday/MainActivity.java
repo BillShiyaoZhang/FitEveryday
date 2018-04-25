0b68106a85182dc91d45b3e8480ff06dc4373206
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2018-04-25 17:46:18 +0800

    0.0

--- app/src/main/java/bill/fiteveryday/MainActivity.java
+++ app/src/main/java/bill/fiteveryday/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,33 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button) findViewById(R.id.button_push_ups);
-        Button button2 = (Button) findViewById(R.id.button_sit_ups);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button_push_ups = (Button) findViewById(R.id.button_push_ups);
+        Button button_sit_ups = (Button) findViewById(R.id.button_sit_ups);
+        button_push_ups.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setNumberDialog();
+                setNumberDialog((TextView) findViewById(R.id.text_push_ups));
+            }
+        });
+        button_sit_ups.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                setNumberDialog((TextView) findViewById(R.id.text_sit_ups));
             }
         });
     }
 
-    private void setNumberDialog() {
+    private void setNumberDialog(final TextView text) {
         AlertDialog.Builder customizeDialog =
                 new AlertDialog.Builder(MainActivity.this);
         final View dialogView = LayoutInflater.from(MainActivity.this)
                 .inflate(R.layout.set_number, null);
-        customizeDialog.setTitle("我是一个自定义Dialog");
+        customizeDialog.setTitle("Input number to be added:");
         customizeDialog.setView(dialogView);
-        customizeDialog.setPositiveButton("确定",
+        customizeDialog.setPositiveButton("OK",
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // 获取EditView中的输入内容
                         EditText edit_text =
                                 (EditText) dialogView.findViewById(R.id.edit_text);
-                        Toast.makeText(MainActivity.this,
-                                edit_text.getText().toString(),
-                                Toast.LENGTH_SHORT).show();
+                        text.setText(edit_text.getText().toString());
                     }
                 });
         customizeDialog.show();
